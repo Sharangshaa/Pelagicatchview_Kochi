7b04c5ddc08363563db96d83b081a8bd1d73b854
Sharang P <sharangshaa@gmail.com>
2025-11-13 21:24:23 +0530

    app.R

--- app.R
+++ app.R
@@ -1,18 +1,23 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
+##read data----------------------------------------
 df <- read.csv("effort_points.csv")
+
+##to set view focused on the location------------------------------------
 mean_lat <- mean(df$latitude, na.rm = TRUE)
 mean_lng <- mean(df$longitude, na.rm = TRUE)
 
+## set color for the gear points on the map ------------------------------
 pal <- colorFactor("viridis", levels = unique(df$gear))
-# Define UI for application 
+
+# Define UI for application--------------------------------------------
 ui <- fluidPage(
 
-    # Application title
+    # Application title--------------------------------------
   titlePanel("Mock fisheries effort map of pelagic fishers"),
 
-    # Sidebar for filtering gear used with a slider input for years
+    # Sidebar for filtering gear used with a slider input for years-------------------------
   sidebarLayout(
     sidebarPanel(
       selectInput("gear", "Select Gear", 
@@ -22,7 +27,7 @@ ui <- fluidPage(
                   value = c(min(df$year), max(df$year)), step = 1, sep = "")
     ),
 
-        # Show a plot of Indian ocean
+        # Show a plot of Indian ocean-----------------------
     mainPanel(
       leafletOutput("map", height = 600)
     )
@@ -30,17 +35,15 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
- ##create reactive object 
   filteredData <- reactive({
     data <- df %>%
-      ##defining slider filter for years
       filter(year >= input$year[1], year <= input$year[2])
     if (input$gear != "All") {
       data <- data %>% filter(gear == input$gear)
     }
     data
   })
-  ## map features and defining default zoom
+  ## define default zoom using mean of lat and longs-----------------------------------------
   output$map <- renderLeaflet({
     leaflet() %>% 
       addTiles() %>% setView(lng = mean_lng, lat = mean_lat,zoom=5)
@@ -53,14 +56,17 @@ server <- function(input, output, session) {
       clearShapes() %>% 
       clearControls() %>%
       addCircles(lng = ~longitude, lat = ~latitude,
-                 ## defining radius of fishing ground based the area covered (buffer is the fishing area explored around the point)
+                 
+                 ## defining radius of fishing ground based the area covered (buffer is the fishing area explored around the point)----------
                  radius = data$buffer,
                  stroke = FALSE, fillOpacity = 0.5,color = ~pal(gear),
-                 ## Show carch details of the point when click through popup
+                 
+                 ## Show catch details of the point when click through popup-----------------------
                  popup = ~paste("<strong> Billfish catch: </strong>", target_volume,"kg", "<br>",
                                 "<strong> Shark catch:</strong>", elasmo_volume,"kg", "<br>",
                                 "<strong> Trip Length:</strong>", trip_day, "days"))  %>%
-                                       
+      
+                   ##legend for the gear on map------------------------------------                    
                   addLegend(
                   position = "bottomright",
                   pal = pal,
@@ -73,3 +79,4 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui = ui, server = server)
+
